acec81568659e1fea9613e1de32295e7b6029dc7
Alexander Kriegisch <alexander@kriegisch.name>
2013-11-18 09:04:08 +0100

    Handle undefined customer to equal all customers in BgControls

--- src/main/java/BgControls.java
+++ src/main/java/BgControls.java
@@ -2,12 +2,18 @@ public class BgControls {
 	public static void forecast(long customer, String month)
 		throws BestellException
 	{
-		System.out.printf("Erzeuge Forecast für Kunde Nr. %d, Monat %s ...%n", customer, month);
+		if (customer == Util.UNDEFINED)
+			System.out.printf("Erzeuge Forecast für alle Kunden, Monat %s ...%n", month);
+		else
+			System.out.printf("Erzeuge Forecast für Kunde Nr. %d, Monat %s ...%n", customer, month);
 	}
 
 	public static void calculation(long customer, String month)
 		throws BestellException
 	{
-		System.out.printf("Erzeuge Kalkulation für Kunde Nr. %d, Monat %s ...%n", customer, month);
+		if (customer == Util.UNDEFINED)
+			System.out.printf("Erzeuge Kalkulation für alle Kunden, Monat %s ...%n", month);
+		else
+			System.out.printf("Erzeuge Kalkulation für Kunde Nr. %d, Monat %s ...%n", customer, month);
 	}
 }
